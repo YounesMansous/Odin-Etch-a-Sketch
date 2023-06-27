fa7bb560f009bfe1a79f0a89d41ad2eaa8f878b1
mansous211 <younes.mansous@etu.unicean.fr>
2023-06-28 00:53:26 +0200

    update

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ let button1 = document.createElement("button");
 let button2 = document.createElement("button");
 let button3 = document.createElement("button");
 let button4 = document.createElement("button");
+let slider = document.createElement("input");
+
+let resize = document.createElement("div");
+let size = document.createElement("div");
+
+resize.classList = "resize";
+size.classList = "size";
+size.style.textAlign = "center";
+size.style.marginBottom = "10px";
 
 button1.textContent = "Color mode";
 button2.textContent = "Rainbow mode";
@@ -34,12 +43,27 @@ button1.classList = "bouton";
 button2.classList = "bouton";
 button3.classList = "bouton";
 button4.classList = "bouton";
-button4.classList = "clear";
+slider.classList = "slider";
+
+slider.type = "range";
+slider.value = 20;
+slider.min = 1;
+slider.max = 64;
+size.textContent = slider.value + "x" + slider.value;
+slider.addEventListener('input', function() {
+    size.textContent = slider.value + "x" + slider.value;
+    console.log(slider.value);
+  }, false);
+
+
+resize.appendChild(size);
+resize.appendChild(slider);
 
 menu.appendChild(button1);
 menu.appendChild(button2);
 menu.appendChild(button3);
 menu.appendChild(button4);
+menu.appendChild(resize);
 
 
 
